099144c3766e7147f17e023b46171bd47e0d6a03
Serhii120796 <sfedyura@ukr.net>
2023-09-08 09:42:42 +0300

    added tn4055

--- products.js
+++ products.js
@@ -1560,6 +1560,17 @@ const goods = [
     "sketch": "./images/catalog/tn/a3/sketch/sketch_tn3054.jpg"
   },
   {
+    "id": "tn3055",
+    "img": "./images/catalog/tn/a3/tn3055.jpg",
+    "article": "ТН3055",
+    "type": "tnA3",
+    "quantityBeads": 0,
+    "quantityHalfBeads": 0,
+    "quantityLuxe": 0,
+    "quantitySHS": 0,
+    "sketch": "./images/catalog/tn/a3/sketch/sketch_tn3055.jpg"
+  },
+  {
     "id": "tnp001",
     "img": "./images/catalog/tn/tapestry/tnp001.jpg",
     "article": "ТНП001",

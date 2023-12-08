ca39b5a20d694a1b17ef4737a7ac9ada8ae98123
Serhii120796 <sfedyura@ukr.net>
2023-12-08 20:46:39 +0200

    1

--- products.js
+++ products.js
@@ -2096,6 +2096,7 @@ const goods = [
     "quantityHalfBeads": 0,
     "quantityLuxe": 0,
     "quantitySHS": 5,
+    "sketch": "./images/catalog/tn/a3/sketch/sketch_tn3069.jpg",
     "date": new Date("2023-10-25")
   },
   {

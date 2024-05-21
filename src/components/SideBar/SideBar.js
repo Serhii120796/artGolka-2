a9d9359d0f396db510640436e78bd9d7e79b6e89
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2024-05-21 05:56:13 +0300

    Update SideBar.js

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,4 +1,4 @@
-import { Aside, Text, ListItem, Button } from "./SideBar.styled";
+import { Aside, Text, ListItem, Button } from './SideBar.styled';
 
 export const Sidebar = ({ abc }) => {
   return (
@@ -6,46 +6,56 @@ export const Sidebar = ({ abc }) => {
       <Text>Каталог товарів</Text>
       <ul>
         <ListItem>
-          <Button type="button" onClick={() => abc("tn")}>Термонаклейки</Button>
+          <Button type="button" onClick={() => abc('tn')}>
+            Термонаклейки
+          </Button>
           <ul>
             <ListItem>
-              <Button type="button" onClick={() => abc("tnA5")}>
+              <Button type="button" onClick={() => abc('tnA5')}>
                 А5
               </Button>
             </ListItem>
             <ListItem>
-              <Button type="button" onClick={() => abc("tnA4")}>
+              <Button type="button" onClick={() => abc('tnA4')}>
                 А4
               </Button>
             </ListItem>
             <ListItem>
-              <Button type="button" onClick={() => abc("tnA3")}>
+              <Button type="button" onClick={() => abc('tnA3')}>
                 А3
               </Button>
             </ListItem>
             <ListItem>
-              <Button type="button" onClick={() => abc("tnA2")}>
+              <Button type="button" onClick={() => abc('tnA2')}>
                 А2
               </Button>
             </ListItem>
             <ListItem>
-              <Button type="button" onClick={() => abc("tnP2060")}>
+              <Button type="button" onClick={() => abc('tnP2060')}>
                 Панно
               </Button>
             </ListItem>
           </ul>
         </ListItem>
         <ListItem>
-          <Button type="button" onClick={() => abc("rps")}>Рушники великодні</Button>
+          <Button type="button" onClick={() => abc('rps')}>
+            Рушники великодні
+          </Button>
         </ListItem>
         <ListItem>
-          <Button type="button" onClick={() => abc("rss")}>Рушники спасівські</Button>
+          <Button type="button" onClick={() => abc('rss')}>
+            Рушники спасівські
+          </Button>
         </ListItem>
         <ListItem>
-          <Button type="button" onClick={() => abc("bow")}>Банти</Button>
+          <Button type="button" onClick={() => abc('bow')}>
+            Банти
+          </Button>
         </ListItem>
         <ListItem>
-          <Button type="button" onClick={() => abc("rvm")}>Рушники обрядові (малі)</Button>
+          <Button type="button" onClick={() => abc('rvm')}>
+            Рушники обрядові (малі)
+          </Button>
         </ListItem>
       </ul>
     </Aside>

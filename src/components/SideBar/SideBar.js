dfc83406d620fb5dce80704e94bea21014ee0719
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2024-05-21 07:14:43 +0300

    Update SideBar.js

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,56 +1,43 @@
 import { Aside, Text, ListItem, Button } from './SideBar.styled';
 
 export const Sidebar = ({ abc }) => {
-  const buttonTypes = ['rps', 'rss', 'bow', 'rvm'];
-
   const categories = {
-    rps: 'Рушники великодні',
-    rss: 'Рушники спасівські',
-    bow: 'Банти',
-    rvm: 'Рушники обрядові (малі)',
+    tn: {
+      name: 'Термонаклейки',
+      list: {
+        tnA5: 'А5',
+        tnA4: 'А4',
+        tnA3: 'А3',
+        tnA2: 'А2',
+        tnP2060: 'Панно',
+      },
+    },
+    rps: { name: 'Рушники великодні' },
+    rss: { name: 'Рушники спасівські' },
+    bow: { name: 'Банти' },
+    rvm: { name: 'Рушники обрядові (малі)' },
   };
 
   return (
     <Aside>
       <Text>Каталог товарів</Text>
       <ul>
-        <ListItem>
-          <Button type="button" onClick={() => abc('tn')}>
-            Термонаклейки
-          </Button>
-          <ul>
-            <ListItem>
-              <Button type="button" onClick={() => abc('tnA5')}>
-                А5
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Button type="button" onClick={() => abc('tnA4')}>
-                А4
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Button type="button" onClick={() => abc('tnA3')}>
-                А3
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Button type="button" onClick={() => abc('tnA2')}>
-                А2
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Button type="button" onClick={() => abc('tnP2060')}>
-                Панно
-              </Button>
-            </ListItem>
-          </ul>
-        </ListItem>
-        {buttonTypes.map(type => (
+        {Object.keys(categories).map(type => (
           <ListItem>
             <Button type="button" onClick={() => abc(type)}>
-              {categories[type]}
+              {categories[type].name}
             </Button>
+            {categories[type].list && (
+              <ul>
+                {Object.keys(categories[type].list).map(subType => (
+                  <ListItem>
+                    <Button type="button" onClick={() => abc(subType)}>
+                      {categories[type].list[subType]}
+                    </Button>
+                  </ListItem>
+                ))}
+              </ul>
+            )}
           </ListItem>
         ))}
       </ul>

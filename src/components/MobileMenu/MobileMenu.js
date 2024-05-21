24fc5ee0a3ff15920be961c347ccdec36ff1abdd
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2024-05-21 06:14:43 +0300

    Update MobileMenu.js

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,13 +1,18 @@
 import { Menu, CloseButton, Icon, Text, Button } from './MobileMenu.styled';
 
 export const MobileMenu = ({ abc, onCloseMenu, statusMenu }) => {
-  const butonTypes = ['rps', 'rss', 'bow', 'rvm'];
+  const buttonTypes = ['rps', 'rss', 'bow', 'rvm'];
 
   const categories = {
     rps: 'Рушники великодні',
     rss: 'Рушники спасівські',
     bow: 'Банти',
     rvm: 'Рушники обрядові (малі)',
+  };
+
+  const handleClick = buttonType => {
+    abc(buttonType);
+    onCloseMenu();
   };
 
   return (
@@ -20,82 +25,40 @@ export const MobileMenu = ({ abc, onCloseMenu, statusMenu }) => {
       <Text>Каталог товарів</Text>
       <ul>
         <li>
-          <Button
-            type="button"
-            onClick={() => {
-              abc('tn');
-              onCloseMenu();
-            }}
-          >
+          <Button type="button" onClick={() => handleClick('tn')}>
             Термоналіпки
           </Button>
           <ul>
             <li>
-              <Button
-                type="button"
-                onClick={() => {
-                  abc('tnA5');
-                  onCloseMenu();
-                }}
-              >
+              <Button type="button" onClick={() => handleClick('tnA5')}>
                 А5
               </Button>
             </li>
             <li>
-              <Button
-                type="button"
-                onClick={() => {
-                  abc('tnA4');
-                  onCloseMenu();
-                }}
-              >
+              <Button type="button" onClick={() => handleClick('tnA4')}>
                 А4
               </Button>
             </li>
             <li>
-              <Button
-                type="button"
-                onClick={() => {
-                  abc('tnA3');
-                  onCloseMenu();
-                }}
-              >
+              <Button type="button" onClick={() => handleClick('tnA3')}>
                 А3
               </Button>
             </li>
             <li>
-              <Button
-                type="button"
-                onClick={() => {
-                  abc('tnA2');
-                  onCloseMenu();
-                }}
-              >
+              <Button type="button" onClick={() => handleClick('tnA2')}>
                 А2
               </Button>
             </li>
             <li>
-              <Button
-                type="button"
-                onClick={() => {
-                  abc('tnP2060');
-                  onCloseMenu();
-                }}
-              >
+              <Button type="button" onClick={() => handleClick('tnP2060')}>
                 Панно
               </Button>
             </li>
           </ul>
         </li>
-        {butonTypes.map(type => (
+        {buttonTypes.map(type => (
           <li>
-            <Button
-              type="button"
-              onClick={() => {
-                abc(type);
-                onCloseMenu();
-              }}
-            >
+            <Button type="button" onClick={() => handleClick(type)}>
               {categories[type]}
             </Button>
           </li>

24c6102771bd00743a71454c1fd46da3ea17412b
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2024-05-28 19:46:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/artGolka-2">
+    <BrowserRouter basename="/artGolka-2/">
       <App />
     </BrowserRouter>
   </React.StrictMode>

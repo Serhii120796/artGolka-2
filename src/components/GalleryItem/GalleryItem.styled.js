518d22df681c850e6e14331dd2d5611863423ee1
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2024-09-26 23:00:55 +0300

    Update GalleryItem.styled.js

--- src/components/GalleryItem/GalleryItem.styled.js
+++ src/components/GalleryItem/GalleryItem.styled.js
@@ -30,7 +30,7 @@ export const Image = css.img`
 @media screen and (max-width: 768px) {
   ${({ $fullscreen }) =>
     $fullscreen
-      ? 'position: fixed; height: auto; width: 100vw;; z-index: 2; top: 0; left: 50%; transform: translateX(-50%);'
+      ? 'position: fixed; height: auto; width: 100vw;; z-index: 2; top: 50%; left: 50%; transform: translate(-5%0, -50%);'
       : 'position: absolut'}
 }
 `;

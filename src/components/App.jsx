2f8173088a304d2aa64142234ec2f8fd7460ecb7
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2025-04-03 21:29:38 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
-import { Layout } from "./Layout/Layout.js";
+import { Layout } from './Layout/Layout.js';
 import { lazy } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
@@ -8,19 +8,18 @@ const Home = lazy(() => import('../pages/Home/Home.js'));
 
 export const App = () => {
   const [searchParams] = useSearchParams();
-  const query = searchParams.get("query");
-const [isOpen, setIsOpen] = useState(query ? false : true);
-  
-  const openMobileMenu = () => setIsOpen(true);
-  const closeMobileMenu = () => setIsOpen(false);
+  const query = searchParams.get('query');
+  const [isOpen, setIsOpen] = useState(query ? false : true);
 
-  //   const toggleMenu = () => {
-  //   setIsOpen(!isOpen);
-  // };
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <Routes>
-      <Route path="/" element={<Layout onOpenMenu={openMobileMenu}/>}>
-        <Route index element={<Home statusMenu={isOpen} onCloseMenu={ closeMobileMenu}/>} />
+      <Route path="/" element={<Layout onOpenMenu={toggleMenu} />}>
+        <Route
+          index
+          element={<Home statusMenu={isOpen} onCloseMenu={toggleMenu} />}
+        />
       </Route>
     </Routes>
   );

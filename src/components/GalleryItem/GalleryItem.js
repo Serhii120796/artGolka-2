55d3d43d3ffeeb616c67a4114bbee1c8472ee8cf
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2025-04-15 19:23:11 +0300

    Move price

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,3 +1,5 @@
+import { price, printPrice } from '../../productPrice';
+import { description } from '../../productDescription';
 import {
   GalleryImgWrapper,
   GalleryWrapper,
@@ -5,117 +7,15 @@ import {
   Text,
 } from './GalleryItem.styled';
 
-export const price = {
-  beads: 18,
-  halfBeads: 10,
-  luxe: 20,
-  shs: 7,
-  tnA2: 340,
-  tnA3: 180,
-  tnA4: 95,
-  tnA5: 50,
-  tnP2060: 180,
-  rps: 140,
-  rpd: 85,
-  rss: 140,
-  bow: 90,
-  rvm: 165,
-  dor: 140,
-  rvs: 240,
-  rvv: 270,
-  svb: 70,
-};
-
-const printPrice = 'А5 - 50 грн., А4 - 95 грн., А3 - 180 грн.';
-
-const description = {
-  tnA2: {
-    title: 'Термонаклейка',
-    desc: '(для вишивки бісером, нитками)',
-    size: '(Розмір 40х60 см)',
-  },
-  tnA3: {
-    title: 'Термонаклейка',
-    desc: '(для вишивки бісером, нитками)',
-    size: '(Розмір 30х40 см)',
-  },
-  tnA4: {
-    title: 'Термонаклейка',
-    desc: '(для вишивки бісером, нитками)',
-    size: '(Розмір 20х30 см)',
-  },
-  tnA5: {
-    title: 'Термонаклейка',
-    desc: '(для вишивки бісером, нитками)',
-    size: '(Розмір 15х20 см)',
-  },
-  tnP2060: {
-    title: 'Термонаклейка',
-    desc: '(для вишивки бісером, нитками)',
-    size: '(Розмір 20х60 см)',
-  },
-  tnPrint: {
-    title: 'Термопринт',
-    desc: '(не вишивається)',
-  },
-  rps: {
-    title: 'Рушник',
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 35х55 см)',
-  },
-  rpd: {
-    title: 'Рушник',
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 26х38 см)',
-  },
-  rss: {
-    title: 'Рушник',
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 35х55 см)',
-  },
-  bow: {
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 13х145 см)',
-  },
-  rvm: {
-    title: 'Рушник малий',
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 24х145 см)',
-  },
-  dor: {
-    title: 'Доріжка',
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 36х59 см)',
-  },
-  rvs: {
-    title: 'Рушник',
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 200х33 см)',
-  },
-  rvv: {
-    title: 'Рушник',
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 200х40 см)',
-  },
-  svb: {
-    title: 'Схема',
-    desc: '(заготовка на габардині)',
-    size: '(Розмір 20х26 см)',
-  },
-  mer: {
-    title: 'Мереживо',
-  },
-};
-
 export const GalleryItem = ({
   item: {
     img,
     article,
     type,
-    quantityBeads,
-    quantityHalfBeads,
-    quantityLuxe,
-    quantitySHS,
+    quantityBeads = 0,
+    quantityHalfBeads = 0,
+    quantityLuxe = 0,
+    quantitySHS = 0,
     sketch,
     size,
     priceItem,
@@ -123,13 +23,18 @@ export const GalleryItem = ({
     title,
   },
 }) => {
+  let costBeads, costSHS;
+  
+  if (quantityBeads || quantityHalfBeads || quantityLuxe) {
+    costBeads =
+      quantityBeads * price.beads +
+      quantityHalfBeads * price.halfBeads +
+      quantityLuxe * price.luxe;
+  }
 
-  const costBeads =
-    quantityBeads * price.beads +
-    quantityHalfBeads * price.halfBeads +
-    quantityLuxe * price.luxe;
-
-  const costSHS = quantitySHS * price.shs;
+  if (quantitySHS) {
+    costSHS = quantitySHS * price.shs;
+  }
 
   return (
     <>
@@ -155,14 +60,14 @@ export const GalleryItem = ({
           <h3>
             {title ? title : description[type].title} {article}
           </h3>
-          <Text>{description[type].desc || desc}</Text>
+          <Text>{desc || description[type].desc}</Text>
           <Text>
             {type !== 'tnPrint'
-              ? `Ціна ${priceItem || price[type]} грн. ${type === "mer" ? "за 1 м.п." : ""} ${size || description[type].size || ''}`
+              ? `Ціна ${priceItem || price[type]} грн. ${type === 'mer' ? 'за 1 м.п.' : ''} ${size || description[type].size || ''}`
               : printPrice}
           </Text>
-          {costBeads ? <Text>Комплект бісеру (+{costBeads} грн)</Text> : ''}
-          {costSHS ? <Text>Комплект ниток СХС (+{costSHS} грн)</Text> : ''}
+          {costBeads && <Text>Комплект бісеру (+{costBeads} грн)</Text>}
+          {costSHS && <Text>Комплект ниток СХС (+{costSHS} грн)</Text>}
         </GalleryWrapper>
       )}
     </>
---
+++ src/productPrice.js
@@ -0,0 +1,22 @@
+export const price = {
+  beads: 18,
+  halfBeads: 10,
+  luxe: 20,
+  shs: 7,
+  tnA2: 340,
+  tnA3: 180,
+  tnA4: 95,
+  tnA5: 50,
+  tnP2060: 180,
+  rps: 140,
+  rpd: 85,
+  rss: 140,
+  bow: 90,
+  rvm: 165,
+  dor: 140,
+  rvs: 240,
+  rvv: 270,
+  svb: 70,
+};
+
+export const printPrice = 'А5 - 50 грн., А4 - 95 грн., А3 - 180 грн.';
